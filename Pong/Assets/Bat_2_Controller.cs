21c30162e519683b8821c2315fb6363f6531fe39
Master-YEET <59836886+Master-YEET@users.noreply.github.com>
2020-09-27 13:05:54 -0400

    Add base game (#2)
    * Finished base game
    
    * Finished base game (2nd commit)

---
+++ Pong/Assets/Bat_2_Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat_2_Controller : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody2D.velocity = new Vector2(0f, 10f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody2D.velocity = new Vector2(0f, -10f);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
